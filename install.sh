c10529081c655432aaa18d723819484d61bb11d9
Manos Lefakis <lefmanos@protonmail.com>
2021-11-25 21:34:41 +0200

    Add ubuntu install function

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+function install_ubuntu() {
+	sudo apt-add-repository ppa:neovim-ppa/unstable
+	sudo apt update
+	sudo apt install neovim
+}
+
 mkdir -p undodir
 nvim -u lib/plug.vim -c ":PlugInstall|:qa!"
