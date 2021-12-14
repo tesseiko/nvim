8f169a1121dd118dc1ec4d8ce8e1fe7231b0ce96
Manos Lefakis <lefmanos@protonmail.com>
2021-12-14 15:00:34 +0200

    update install script

--- install.sh
+++ install.sh
@@ -6,12 +6,30 @@ install_ubuntu() {
 	sudo apt install neovim
 }
 
-mkdir -p undodir
-[ ! -f lib_plug/servers.lua ] && echo "
--- edit the line bellow eg:
--- local servers = { 'gopls', 'yamlls', 'tsserver', 'bashls' }
-local servers = {}
-return servers
-" > lib_plug/servers.lua
+local_install() {
+	mkdir -p undodir
+	[ ! -f lib_plug/servers.lua ] && echo "
+	-- edit the line bellow eg:
+	-- local servers = { 'gopls', 'yamlls', 'tsserver', 'bashls' }
+	local servers = {}
+	return servers
+	" > lib_plug/servers.lua
 
-nvim -u lib/plug.vim -c ":PlugInstall|:qa!"
+	nvim -u lib/plug.vim -c ":PlugInstall|:qa!"
+}
+
+if [ $# -eq 1 ]; then
+	if [ $1 = "install-ubuntu" ]; then
+		install_ubuntu
+		local_install
+		exit 0
+	else
+		echo "$0 [help|install-ubuntu]"
+		exit 0
+	fi
+else
+	echo "$0 [help|install-ubuntu]"
+	exit 0
+fi
+
+local_install
