3eec3bf89e570ff93ee8778119d67617e69f643e
lefmanos <lefmanos@protonmail.com>
2022-12-28 20:03:05 +0200

    Add package manager and clean up directories

---
+++ _oldconf/lib_plug/servers.lua
@@ -0,0 +1,6 @@
+
+-- edit the line bellow eg:
+-- local servers = { 'gopls', 'clangd', 'yamlls', 'tsserver', 'bashls' }
+local servers = { 'gopls', 'clangd', 'yamlls', 'tsserver', 'bashls', 'pylsp', 'texlab'}
+return servers
+
---
+++ init.lua
@@ -0,0 +1,14 @@
+-- Install packer
+local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+local is_bootstrap = false
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+	is_bootstrap = true
+	vim.fn.system { 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path }
+	vim.cmd [[packadd packer.nvim]]
+end
+
+require('packer').startup(function(use)
+	-- Package manager
+	use 'wbthomason/packer.nvim'
+
+end)
