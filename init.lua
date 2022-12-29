11c7e279a9a0da3dd9b1f737815c0df36c195a75
lefmanos <lefmanos@protonmail.com>
2022-12-29 02:50:39 +0200

    done

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ vim.g.mapleader = ' '
 require("package_manager")
 require("settings")
 require("completions")
+require("remaps")
 vim.cmd [[colorscheme dracula]]
 
 
---
+++ lua/remaps/init.lua
@@ -0,0 +1,159 @@
+vim.cmd([[
+
+nnoremap Y y$
+nnoremap n nzzzv
+nnoremap N Nzzzv
+nnoremap J m`J``
+
+vnoremap <leader>i <C-a>
+vnoremap g<leader>i g<C-a>
+nnoremap <leader>i <C-a>
+
+" undo breakpoints
+inoremap , ,<C-g>u
+inoremap . .<C-g>u
+inoremap ; ;<C-g>u
+
+" moving
+nnoremap <leader>k :m .-2<cr>==
+nnoremap <leader>j :m .+1<cr>==
+vnoremap K :m '<-2<cr>gv=gv
+vnoremap J :m '>+1<cr>gv=gv
+inoremap <C-l> <right>
+inoremap <C-j> <down>
+inoremap <C-k> <up>
+inoremap <C-h> <left>
+inoremap <C-M-e> € 
+
+" file navigation
+nnoremap <silent> <leader>? :tabnew ~/vimwiki/src/vim.md<cr>
+nnoremap <silent> <leader>rc :tabnew ~/.config/nvim/init.vim<cr>
+nnoremap <silent> <C-n> :bnext<cr>
+nnoremap <silent> <C-p> :bprev<cr>
+nnoremap <silent> <C-q> :b#<bar>bd#<cr>
+
+" etc
+nnoremap <leader>m i<@><esc> 
+nnoremap <leader>M /<@><cr>
+nnoremap <leader><tab> /<@><cr>3cl
+nnoremap <silent> <leader><C-h> :tabprev<cr>
+nnoremap <silent> <leader><C-l> :tabnext<cr>
+nnoremap <leader>d dlp
+nnoremap - <C-W><
+nnoremap + <C-W>>
+nnoremap <C--> <C-W>-
+nnoremap <C-+> <C-W>+
+inoremap :w<cr> <esc>:w<cr>
+tnoremap <esc><esc> <C-\><C-n>
+tnoremap <C-w> <C-\><C-n><C-w>
+nnoremap <C-l> <C-w>l
+nnoremap <C-h> <C-w>h
+nnoremap <silent><C-k> :cprev<cr> 
+nnoremap <silent><C-j> :cnext<cr>
+nnoremap <C-s> :w<cr>
+inoremap <C-s> <esc>:w<cr>
+nnoremap <leader>dp :diffput<cr>
+nnoremap <leader>dg :diffget<cr>
+nnoremap <leader>dh :diffget 1<cr>
+nnoremap <leader>dl :diffget 3<cr>
+nnoremap <silent><leader>lq :LspStop<cr>
+
+" surround current line with empty lines and reverse
+nnoremap <leader>s O<esc>jo<esc>k
+nnoremap <leader>S kddjddk
+
+]])
+
+vim.cmd([[
+map α a
+map ς w
+map ε e
+map ρ r
+map τ t
+map υ y
+map θ u
+map ι i
+map ο o
+map π p
+map σ s
+map δ d
+map φ f
+map γ g
+map η h
+map ξ j
+map κ k
+map λ l
+map ζ z
+map χ x
+map ψ c
+map ω v
+map β b
+map ψ c
+map μ m
+map ν n
+
+map Α A
+map Σ W
+map Ε E
+map Ρ R
+map Τ T
+map Υ Y
+map Θ U
+map Ι I
+map Ο O
+map Π P
+map Σ S
+map Δ D
+map Φ F
+map Γ G
+map Η H
+map Ξ J
+map Κ K
+map Λ L
+map Ζ Z
+map Χ X
+map Ψ C
+map Ω V
+map Β B
+map Ψ C
+map Μ M
+map Ν Ν
+
+nmap γ;ιπ gqip
+nmap ψς cw
+nmap ψις ciw
+nmap δς dw
+]])
+
+-- commends
+vim.cmd([[
+
+" comments
+autocmd filetype css 	nnoremap <buffer> <leader>c I/* <esc>A */<esc>j
+autocmd filetype css 	nnoremap <buffer> <leader>cc dipO/*<esc>p}O*/<esc>
+autocmd filetype css 	nnoremap <buffer> <leader>C ^3dlf*dh2dl0j
+autocmd filetype css 	nnoremap <buffer> <leader>cC {jdd}kdd
+autocmd filetype go 	nnoremap <buffer> <leader>c I// <esc>j
+autocmd filetype go 	nnoremap <buffer> <leader>C ^3dlj
+autocmd filetype c 		nnoremap <buffer> <leader>c I// <esc>j
+autocmd filetype c	 	nnoremap <buffer> <leader>cc dipO/*<esc>p}O*/<esc>
+autocmd filetype c 		nnoremap <buffer> <leader>C ^3dlj
+autocmd filetype c	 	nnoremap <buffer> <leader>cC {jdd}kdd
+autocmd filetype cpp	nnoremap <buffer> <leader>c I// <esc>j
+autocmd filetype cpp 	nnoremap <buffer> <leader>cc dipO/*<esc>p}O*/<esc>
+autocmd filetype cpp	nnoremap <buffer> <leader>C ^3dlj
+autocmd filetype cpp 	nnoremap <buffer> <leader>cC {jdd}kdd
+autocmd filetype sh 	nnoremap <buffer> <leader>c I# <esc>j
+autocmd filetype sh 	nnoremap <buffer> <leader>C ^2dlj
+autocmd filetype zsh 	nnoremap <buffer> <leader>c I# <esc>j
+autocmd filetype zsh 	nnoremap <buffer> <leader>C ^2dlj
+autocmd filetype conf 	nnoremap <buffer> <leader>c I# <esc>j
+autocmd filetype conf 	nnoremap <buffer> <leader>C ^2dlj
+autocmd filetype vim 	nnoremap <buffer> <leader>c I" <esc>j
+autocmd filetype vim 	nnoremap <buffer> <leader>C ^2dlj
+autocmd filetype python nnoremap <buffer> <leader>c I# <esc>j
+autocmd filetype python nnoremap <buffer> <leader>C ^2dlj
+autocmd filetype haskell	nnoremap <buffer> <leader>c I-- <esc>j
+autocmd filetype haskell	nnoremap <buffer> <leader>C ^3dlj
+
+]])
