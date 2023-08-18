86f492ff7eae2a48b7398fe54488e2a3c2cb5229
Manos Lefakis <lefmanos@protonmail.com>
2023-08-18 18:05:30 +0300

    migrate completions

--- lua/completions.lua
+++ lua/completions.lua
@@ -1,36 +1,34 @@
+local opts = { noremap=true, silent=true }
 
-vim.cmd([[
-" basic completions
-inoremap ( ()<left>
-inoremap [ []<left>
-inoremap " ""<left>
-inoremap ' ''<left>
-autocmd FileType sh 	inoremap <buffer>  ` ``<left>
-autocmd FileType sh 	vnoremap <buffer>  <leader>` di`<esc>pa`<esc>
-autocmd FileType vim 	inoremap <buffer>  < <><left>
-autocmd FileType vim 	vnoremap <buffer>  <leader>, di<lt><esc>pa><esc>
-inoremap { {}<left>
-" probably this should remaped only on C files (also java,js if I used them)
-inoremap }} <ESC>/}<cr>i<cr><ESC>O
-inoremap )) <ESC>/)<cr>i<cr><ESC>O
-" single line surrounds
-vnoremap <leader>( hdi()<esc>Pldlp
-vnoremap <leader>[ hdi[]<esc>Pldlp
-vnoremap <leader>" hdi""<esc>Pldlp
-vnoremap <leader>' hdi''<esc>Pldlp
-vnoremap <leader>{ hdi{}<esc>Pldlp
+-- basic completions
+vim.keymap.set('i', '(', '()<left>', opts)
+vim.keymap.set('i', '[', '[]<left>', opts)
+vim.keymap.set('i', '"', '""<left>', opts)
+vim.keymap.set('i', '\'', '\'\'<left>', opts)
 
-vnoremap <leader><c-b> hdi{}<esc>Pldlp%i\textbf<esc>
+vim.api.nvim_create_autocmd("filetype", {
+    pattern = "sh",
+    callback = function()
+        vim.keymap.set('i', '`', '``<left>', opts)
+        vim.keymap.set('v', '<leader>`', 'di`<esc>pa`<esc>', opts)
+    end,
+})
 
-" advanced completions
-autocmd filetype html 	inoremap <buffer> < <><left>
-autocmd filetype html 	noremap <buffer> <silent> <esc> <esc>:wa<cr>
-autocmd filetype css 	noremap <buffer> <silent> <esc> <esc>:wa<cr>
-autocmd filetype html 	inoremap <buffer>  >> <esc>ya<ea!@#<esc>f>pF<a/<left><left><CR>!@#<CR><esc>?!@#<cr>n3cl
-autocmd filetype html 	inoremap <buffer>  <leader><tab> <esc>/!@#<cr>3cl
+vim.keymap.set('i', '{', '{}<left>', opts)
 
-autocmd filetype typescriptreact 	inoremap <buffer> < <><left>
-autocmd filetype typescriptreact 	noremap <buffer> <silent> <esc> <esc>:wa<cr>
-autocmd filetype typescriptreact 	inoremap <buffer>  >> <esc>ya<ea!@#<esc>f>pF<a/<left><left><CR>!@#<CR><esc>?!@#<cr>n3cl
-autocmd filetype typescriptreact 	inoremap <buffer>  <leader><tab> <esc>/!@#<cr>3cl
-]])
+-- single line surrounds
+vim.keymap.set('v', '<leader>(', 'hdi()<esc>Pldlp', opts)
+vim.keymap.set('v', '<leader>[' , 'hdi[]<esc>Pldlp', opts)
+vim.keymap.set('v', '<leader>"' , 'hdi""<esc>Pldlp', opts)
+vim.keymap.set('v', '<leader>\'' , 'hdi\'\'<esc>Pldlp', opts)
+vim.keymap.set('v', '<leader>{' , 'hdi{}<esc>Pldlp', opts)
+
+vim.api.nvim_create_autocmd("filetype", {
+    pattern = {"html", "typescriptreact"},
+    callback = function()
+        vim.keymap.set('i', '<', '<><left>', opts)
+        vim.keymap.set('i', '>>', '<esc>ya<ea!@#<esc>f>pF<a/<left><left><CR>!@#<CR><esc>?!@#<cr>n3cl', opts)
+        vim.keymap.set('i', '<leader><tab>', '<esc>/!@#<cr>3cl', opts)
+    end,
+})
+
