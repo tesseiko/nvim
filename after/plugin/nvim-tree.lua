bbba80e6b8d51dcf49268a57e9ed5c63df0d8b90
lefmanos <lefmanos@protonmail.com>
2023-10-25 18:32:40 +0300

    update nvim tree

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -6,22 +6,27 @@
   vim.opt.termguicolors = true
 
   -- empty setup using defaults
-  require("nvim-tree").setup()
+  -- require("nvim-tree").setup()
 
   -- OR setup with some options
   require("nvim-tree").setup({
-    sort = {
-      sorter = "case_sensitive",
-    },
-    view = {
-      width = 30,
-    },
-    renderer = {
-      group_empty = true,
-    },
-    filters = {
-      dotfiles = true,
-    },
+      sort = {
+          sorter = "case_sensitive",
+      },
+      view = {
+          width = 30,
+      },
+      renderer = {
+          group_empty = true,
+      },
+      update_focused_file = {
+          enable = true,
+          update_root = true
+      },
+      filters = {
+          dotfiles = true,
+      },
   })
 
 vim.keymap.set('n', '<leader>E', [[:NvimTreeToggle<cr>]], {silent = true})
+
