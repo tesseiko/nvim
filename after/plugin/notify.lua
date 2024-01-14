74fab42f13496093bd96ea8236837a91d670630f
lefmanos <lefmanos@protonmail.com>
2024-01-14 02:42:45 +0200

    Dismiss notification remaps

--- after/plugin/notify.lua
+++ after/plugin/notify.lua
@@ -2,3 +2,4 @@ local telescope = require("telescope")
 telescope.load_extension("notify")
 
 vim.api.nvim_set_keymap("n", "<leader>tn", '<cmd> lua require("telescope").extensions.notify.notify()<CR>', {noremap = true, silent = true})
+vim.keymap.set('n', '<leader>tq', '<cmd> lua require("notify").dismiss({})<CR>', { noremap = true, silent = true })
